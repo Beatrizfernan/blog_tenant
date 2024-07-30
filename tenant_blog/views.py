8a412427244a82c00066d8393298c39ed494e07f
Beatriz <beaultfernandes@gmail.com>
2024-07-30 16:08:42 -0300

    removendo a view de detalhe

--- tenant_blog/views.py
+++ tenant_blog/views.py
@@ -4,21 +4,15 @@ from .models import Person
 
 class PersonListView(ListView):
     model = Person
-    template_name = 'people/person_list.html'
+    template_name = 'tenant_blog/person_list.html'
 
     def get_queryset(self):
         return Person.objects.filter(tenant=self.request.tenant)
 
-class PersonDetailView(DetailView):
-    model = Person
-    template_name = 'people/person_detail.html'
-
-    def get_queryset(self):
-        return Person.objects.filter(tenant=self.request.tenant)
 
 class PersonCreateView(CreateView):
     model = Person
-    template_name = 'people/person_form.html'
+    template_name = 'tenant_blog/person_form.html'
     fields = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'date_of_birth']
 
     def form_valid(self, form):
@@ -27,7 +21,7 @@ class PersonCreateView(CreateView):
 
 class PersonUpdateView(UpdateView):
     model = Person
-    template_name = 'people/person_form.html'
+    template_name = 'tenant_blog/person_form.html'
     fields = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'date_of_birth']
 
     def get_queryset(self):
@@ -35,7 +29,7 @@ class PersonUpdateView(UpdateView):
 
 class PersonDeleteView(DeleteView):
     model = Person
-    template_name = 'people/person_confirm_delete.html'
+    template_name = 'tenant_blog/person_confirm_delete.html'
     success_url = reverse_lazy('person-list')
 
     def get_queryset(self):
