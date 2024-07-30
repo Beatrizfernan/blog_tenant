1d1349cb3e5813df003ef9002231a8e16fbe4c83
Beatriz <beaultfernandes@gmail.com>
2024-07-30 16:05:12 -0300

    crinado o model person

--- tenant_blog/models.py
+++ tenant_blog/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 from tenant_schemas.models import TenantMixin
 
-class Client(TenantMixin):
-    name = models.CharField(max_length=100)
-    paid_until =  models.DateField()
-    on_trial = models.BooleanField()
+class Tenant(TenantMixin):
+    name = models.CharField(max_length=100, unique=True)
     created_on = models.DateField(auto_now_add=True)
 
-    # default true, schema will be automatically created and synced when it is saved
-    auto_create_schema = True
+    class Meta:
+        verbose_name = 'Tenant'
+        verbose_name_plural = 'Tenants'
+
+class Person(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, related_name='people')
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
