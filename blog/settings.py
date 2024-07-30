0a1a0e36f2f84a19c90a3e5a217a6d6da0868cf6
Beatriz <beaultfernandes@gmail.com>
2024-07-30 10:45:51 -0300

    finalizações

--- blog/settings.py
+++ blog/settings.py
@@ -49,7 +49,7 @@ SHARED_APPS = (
 
 TENANT_MODEL = "tenant_blog.Client" 
 MIDDLEWARE = [
-   
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,10 +139,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FILE_STORAGE = 'tenant_schemas.storage.TenantFileSystemStorage'
+ALLOWED_HOSTS = [
+    '127.0.0.1', 
+    'localhost',
+    'blogdaBia.Beatriz.com',
+    'blogdaDani.Beatriz.com',
+]
