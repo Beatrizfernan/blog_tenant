b2f35b784e16f1a0e841ac96595608dbc19c29bc
Beatriz <beaultfernandes@gmail.com>
2024-07-30 16:06:02 -0300

    criando as views do crud

--- tenant_blog/views.py
+++ tenant_blog/views.py
@@ -1,5 +1,42 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Person
 
-class HomeView(TemplateView):
-    template_name = 'tenant_blog/home.html'
+class PersonListView(ListView):
+    model = Person
+    template_name = 'people/person_list.html'
 
+    def get_queryset(self):
+        return Person.objects.filter(tenant=self.request.tenant)
+
+class PersonDetailView(DetailView):
+    model = Person
+    template_name = 'people/person_detail.html'
+
+    def get_queryset(self):
+        return Person.objects.filter(tenant=self.request.tenant)
+
+class PersonCreateView(CreateView):
+    model = Person
+    template_name = 'people/person_form.html'
+    fields = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'date_of_birth']
+
+    def form_valid(self, form):
+        form.instance.tenant = self.request.tenant
+        return super().form_valid(form)
+
+class PersonUpdateView(UpdateView):
+    model = Person
+    template_name = 'people/person_form.html'
+    fields = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'date_of_birth']
+
+    def get_queryset(self):
+        return Person.objects.filter(tenant=self.request.tenant)
+
+class PersonDeleteView(DeleteView):
+    model = Person
+    template_name = 'people/person_confirm_delete.html'
+    success_url = reverse_lazy('person-list')
+
+    def get_queryset(self):
+        return Person.objects.filter(tenant=self.request.tenant)
